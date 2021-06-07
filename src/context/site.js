37ad27df221ce206052e18986e682c9107fd2059
daneng1 <engeldb@gmail.com>
2021-06-06 17:43:23 -0700

    added third progress status, got name, task and diffuculty filter to work. Still can't get status filter to work

--- src/context/site.js
+++ src/context/site.js
@@ -7,11 +7,14 @@ function SiteProvider(props) {
   const [currentPage, setCurrentPage] = useState(1);
   const [pages, setPages] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [sortSelected, setSortSelected] = useState('');
   const state = {
     list,
     setList,
     pages,
     setPages,
+    sortSelected,
+    setSortSelected,
     currentPage,
     setCurrentPage,
     loading,
