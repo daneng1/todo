8b233127732ce70cf21cd4bc0cd25a447bed02ed
daneng1 <engeldb@gmail.com>
2021-06-09 08:32:43 -0700

    finished filter

--- src/components/list.js
+++ src/components/list.js
@@ -15,6 +15,7 @@ export default function TodoList(props) {
   const [id, setId] = useState('');
   const [value, setValue] = useState('');
   const [handleSubmit] = useForm(todoList);
+  const [filter, setFilter] = useState('all');
   const [goToNextPage, goToPreviousPage, changePage, getPaginatedData, getPaginationGroup] = usePagination();
   const [show, setShow] = useState(false);
 
@@ -25,7 +26,11 @@ export default function TodoList(props) {
     setOpen(!open);
     setId(id);
   }
+
+  // TODO: new state variable starting as All, tied to dropdown,
+  // then run through if statements below to set filteredList
   let list = context.list;
+  let filteredList = list;
 
   function todoList(todo) {
     setValue(todo);
@@ -33,18 +38,16 @@ export default function TodoList(props) {
   }
 
   // sort by pending, completed or all
-  function sortCompleted(val) {
-    console.log(val);
-    if (val === "complete") {
-      list = list.filter(item => item.completed === "complete");
-    } else if (val === "pending") {
-      list = list.filter(item => item.completed === "pending");
-    } else if (val === "in-progress") {
-      list = list.filter(item => item.completed === "in-progress");
-    } else if (val === "all") {
-      list = context.list;
+    if (filter === "complete") {
+      filteredList = list.filter(item => item.completed === "complete");
+    } else if (filter === "pending") {
+      filteredList = list.filter(item => item.completed === "pending");
+    } else if (filter === "in-progress") {
+      filteredList =  list.filter(item => item.completed === "in-progress");
+    } else if (filter === "all") {
+      filteredList =  context.list;
     }
-  }
+
 
   function status(val) {
     if (val === 'pending') {
@@ -58,24 +61,28 @@ export default function TodoList(props) {
 
 
   // sort by text, name and difficulty
+  console.log(filteredList);
   if (context.sortSelected === 'name') {
-    list.sort((a, b) => {
+    filteredList.sort((a, b) => {
       if (a.assignee.toLowerCase() > b.assignee.toLowerCase()) return 1;
       if (a.assignee.toLowerCase() < b.assignee.toLowerCase()) return -1;
       return 0;
     })
+    console.log(filteredList);
+
   } else if (context.sortSelected === 'difficulty') {
-    list.sort((a, b) => {
+    filteredList.sort((a, b) => {
       if (a.difficulty > b.difficulty) return 1;
       if (a.difficulty < b.difficulty) return -1;
       return 0;
     })
   } else if (context.sortSelected === 'task') {
-    list.sort((a, b) => {
+     filteredList = filteredList.sort((a, b) => {
       if (a.text.toLowerCase() > b.text.toLowerCase()) return 1;
       if (a.text.toLowerCase() < b.text.toLowerCase()) return -1;
       return 0;
     })
+    console.log(filteredList);
   }
   // console.log('inside sortSelected', context.sortSelected);
 
@@ -88,13 +95,13 @@ export default function TodoList(props) {
         <option value="difficulty">Difficulty</option>
         <option value="task">Task</option>
       </select>
-      <select onChange={(e) => sortCompleted(e.target.value)}>
+      <select onChange={(e) => setFilter(e.target.value)}>
         <option value="all">All</option>
         <option value="complete">Complete</option>
         <option value="in-progress">In-progress</option>
         <option value="pending">Pending</option>
       </select>
-      {getPaginatedData(list).map((item) => (
+      {getPaginatedData(filteredList).map((item) => (
         <div>
           <Modal.Dialog style={{ width: '40vw' }} key={item._id} className="m-2 shadow">
             <Modal.Header >
